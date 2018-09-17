b668c251ff1cc8a9d79953fed8ee13622e05613b
褚德权 <chudequan_48@163.com>
2018-09-17 17:29:02 +0800

    分散式录房界面

--- src/views/hostingEntryHouse/index.vue
+++ src/views/hostingEntryHouse/index.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    12312312312
+  <div class="app-container">
+    <el-tabs type="border-card">
+      <el-tab-pane label="新建分散式整租">
+        <div
+          ref="entryHouseContainer"
+          class="entry-house-container"
+          :style="{height: mainHeight + 'px'}">
+          123132131212
+        </div>
+        <div class="entry-house-bottom">
+          <el-button
+            type="primary"
+            size="small">保存并继续添加</el-button>
+          <el-button size="small">确定</el-button>
+          <el-button size="small">取消</el-button>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+import { debounce } from "@/utils"
 export default {
-
+  data () {
+    return {
+      mainHeight: 500
+    }
+  },
+  mounted () {
+    let changeMainHeight = debounce(() => {
+      this.mainHeight = Math.max(document.body.clientHeight - 200, 250)
+    }, 100)
+    changeMainHeight()
+    window.addEventListener("resize", changeMainHeight)
+  }
 }
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+  .entry-house-container {
+    overflow-y: scroll;
+  }
+  .entry-house-bottom {
+    text-align: right;
+  }
 </style>
