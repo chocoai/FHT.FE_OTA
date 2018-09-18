ffbcca8969b3c52182a40c73b605ab7e061dd88a
dengdongming <ddm@fuhengkeji.com>
2018-09-18 15:50:41 +0800

    出租状态

--- src/api/houseManage.js
+++ src/api/houseManage.js
@@ -2,25 +2,21 @@ import { fetch } from '@/utils/fetch'
 
 export const houseAsyncApi = {
   requestPath: 'queryHostingHouseInfo',
-  queryMethod: 'list',
   isMock: true,
   queryActivityListByPage (params = {}) {
     return fetch(this.requestPath, {
-      // method: this.queryMethod,
       params
     }, {
       isMock: this.isMock
     })
   }
 }
-/* 发布撤销房源 */
-export function publishHouseApi (params, type) {
-  return fetch({
-    url: '/rooms/published',
-    method: 'post',
-    data: {
-      method: type === 1 ? 'publish' : 'unpublish', // 1.发布 2.撤销
-      params
-    }
+/* 修改出租状态 */
+export function changeRoomStatusApi (params = {}) {
+  return fetch('changeLeaseStatus', {
+    method: '',
+    params
+  }, {
+    isMock: true
   })
 }
