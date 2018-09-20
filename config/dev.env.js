b21919772967bae94901fe2ebf7c6486f2547c67
王玲波 <454949876@qq.com>
2018-09-20 12:57:04 +0800

    ### *柏林*

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,7 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:29:26
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-09-17 11:13:48
+ * @Last Modified time: 2018-09-20 12:56:51
  */
 
 module.exports = {
@@ -11,6 +11,6 @@ module.exports = {
   NPM_CONFIG_REPORT: '"false"',
   /* 本地请求时配置api代理 */
   // BASE_API: '"http://localhost:9528/api/"',
-  BASE_API: '"https://dev.mdguanjia.com/bop/"',
-  MOCK: '"true"'
+  BASE_API: '"https://dev.mdguanjia.com/otastarter/"',
+  MOCK: 'true'
 }
