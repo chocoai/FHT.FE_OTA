739df50bc1538c8b351126986cf4457e30d797da
王玲波 <454949876@qq.com>
2018-09-25 14:38:56 +0800

    ### *柏林* 【打包资源优化】开启gzip

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,18 +2,16 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 17:11:02
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-09-17 20:52:04
+ * @Last Modified time: 2018-09-25 14:21:36
  */
 
 <template>
   <div class="layout-container">
     <p style="margin-top: 0;">{{ roles | rolesFilter }}{{ name }}</p>
-    <div class="img-banner"/>
   </div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
-import bannerPic from '@/assets/banner1.jpg'
 export default {
   name: 'Dashboard',
   filters: {
@@ -24,11 +22,6 @@ export default {
         'service': '【服务用户】'
       }
       return valueMap[value] || '【普通用户】'
-    }
-  },
-  data () {
-    return {
-      bannerPic: bannerPic
     }
   },
   computed: {
@@ -45,12 +38,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.img-banner {
-  height: 50vh;
-  border-radius: 10px;
-  background-color: #2d3a4b;
-  background-image: url(../../assets/banner1.jpg);
-  background-size: cover;
-  background-position: center center;
-}
+
 </style>
