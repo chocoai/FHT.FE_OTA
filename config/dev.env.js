1242458ca105905b3a6d226fafb9060e96dffba3
王玲波 <454949876@qq.com>
2018-10-15 11:42:51 +0800

    ### *柏林* OTA配置dev、prod环境打包

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,11 +2,11 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:29:26
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-09-25 21:08:18
+ * @Last Modified time: 2018-10-15 11:40:29
  */
 
 module.exports = {
-  NODE_ENV: '"development"',
+  NODE_ENV: '"production"',
   ENV_CONFIG: '"dev"',
   NPM_CONFIG_REPORT: '"false"',
   /* 本地请求时配置api代理 */
