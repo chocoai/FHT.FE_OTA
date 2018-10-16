ad566d8b54b7291d3308e26c6504b03b547aa0c1
王玲波 <454949876@qq.com>
2018-10-16 13:53:49 +0800

    ### *柏林* dev => development

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,11 +2,11 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:29:26
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-10-15 11:40:29
+ * @Last Modified time: 2018-10-16 13:53:14
  */
 
 module.exports = {
-  NODE_ENV: '"production"',
+  NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   NPM_CONFIG_REPORT: '"false"',
   /* 本地请求时配置api代理 */
