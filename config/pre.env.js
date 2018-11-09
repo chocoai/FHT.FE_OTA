9940d9791331abb63d5d3e73c6dadd105a4db29f
王玲波 <454949876@qq.com>
2018-11-09 15:29:51 +0800

    ### *柏林*

--- config/pre.env.js
+++ config/pre.env.js
@@ -2,12 +2,12 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:29:26
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-11-09 15:28:21
+ * @Last Modified time: 2018-11-09 15:27:39
  */
 
 module.exports = {
   NODE_ENV: '"production"',
-  ENV_CONFIG: '"sit"',
+  ENV_CONFIG: '"pre"',
   NPM_CONFIG_REPORT: '"false"',
-  BASE_API: '"//test1.mdguanjia.com/otastarter/"'
+  BASE_API: '"//pre.mdguanjia.com/otastarter/"'
 }
--- config/test.env.js
+++ config/test.env.js
@@ -2,12 +2,12 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:29:26
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-11-09 15:28:21
+ * @Last Modified time: 2018-11-09 15:28:26
  */
 
 module.exports = {
   NODE_ENV: '"production"',
-  ENV_CONFIG: '"sit"',
+  ENV_CONFIG: '"test"',
   NPM_CONFIG_REPORT: '"false"',
-  BASE_API: '"//test1.mdguanjia.com/otastarter/"'
+  BASE_API: '"//test.mdguanjia.com/otastarter/"'
 }
